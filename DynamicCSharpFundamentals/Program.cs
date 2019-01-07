2219fd8eb4525f25d3c07ff7f8bdbb66a142d524
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-01-07 22:51:54 +0100

    -- Dynamic C# Fundamentals -- --- Introduction Dynamic C# -- - Dynamic Binding in Action and RunTimeBinderException

--- DynamicCSharpFundamentals/Program.cs
+++ DynamicCSharpFundamentals/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using System.Globalization;
+
 
 namespace DynamicCSharpFundamentals
 {
@@ -10,6 +13,36 @@ namespace DynamicCSharpFundamentals
     {
         static void Main(string[] args)
         {
+            OutputTimeStaticBinding();
+            OutputTimeDynamicBinding();
+            OutputTimeDynamicBindingRunTimeError();
+
+            WriteLine("\n\nPress enter to exit...");
+            ReadLine();
+        }
+
+        private static void OutputTimeStaticBinding()
+        {
+            DateTime dt = DateTime.Now;                       
+            string time = dt.ToLongDateString();
+
+            WriteLine(time);
+        }
+
+        private static void OutputTimeDynamicBinding()
+        {
+            dynamic dt = DateTime.Now;
+            string time = dt.ToLongDateString();
+
+            WriteLine(time);
+        }
+
+        private static void OutputTimeDynamicBindingRunTimeError()
+        {
+            dynamic dt = DateTime.Now;
+            string time = dt.IsItCoffeeTime();
+
+            WriteLine(time);
         }
     }
 }
