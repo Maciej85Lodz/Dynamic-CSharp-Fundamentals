b6e930a0dae4b78e403bb748e223d0cb85c08937
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-01-08 19:32:31 +0100

    - Dynamic C# Fundamentals - -- Introduction to Dynamic C# -- --- RunTime Method Resolution ---

--- DynamicCSharpFundamentals/Program.cs
+++ DynamicCSharpFundamentals/Program.cs
@@ -12,34 +12,76 @@ namespace DynamicCSharpFundamentals
             //OutputTimes.OutputTimeDynamicBinding();
             //OutputTimes.OutputTimeDynamicBindingRunTimeError();
 
+            //int i = 42;
+            //dynamic di = i;
+            //int i2 = di;
+            //WriteLine($"i = {i} di = {di} i2 = {i2}");
+
+
+            ////string s = "hello";
+            ////dynamic ds = s;
+            ////int x = ds;
+            //long l = 99;
+            //dynamic dl = l;
+            //int y = (int)dl;
+
+
+            //WriteLine($"l = {l} dl = {dl} y = {y}");
+
+            //dynamic z = "Hi Maciej";
+
+            //WriteLine($"z is a {z.GetType()} = {z}");
+
+            //z = 42;
+            //WriteLine($"z is a {z.GetType()} = {z}");
+
+
             int i = 42;
-            dynamic di = i;
-            int i2 = di;
-            WriteLine($"i = {i} di = {di} i2 = {i2}");
+            PrintMe(i);  // --> PrintMe(int value) static binding
 
+            dynamic d;
+            //WriteLine("Create [i]nt or [d]ouble");
+            //ConsoleKeyInfo choice = ReadKey(intercept: true);
+            //if (choice.Key == ConsoleKey.I)
+            //{
+            //    d = 99;
+            //}
+            //else
+            //{
+            //    d = 55.5;
+            //}
+            //PrintMe(d);
 
-            //string s = "hello";
-            //dynamic ds = s;
-            //int x = ds;
-            long l = 99;
-            dynamic dl = l;
-            int y = (int)dl;
+            d = long.MaxValue;
+            PrintMe(d);
 
-
-            WriteLine($"l = {l} dl = {dl} y = {y}");
-
-            dynamic z = "Hi Maciej";
-
-            WriteLine($"z is a {z.GetType()} = {z}");
-
-            z = 42;
-            WriteLine($"z is a {z.GetType()} = {z}");
-
+            d = "Hi Maciej";
+            PrintMe(d);
 
             WriteLine("\n\nPress enter to exit...");
             ReadLine();
         }
 
-        
+        static void PrintMe(int value)
+        {
+            WriteLine($"PrintMe(int) called value: {value}");
+        }
+
+        static void PrintMe(long value)
+        {
+            WriteLine($"PrintMe(long) called value: {value}");
+        }
+
+        static void PrintMe(dynamic value)
+        {
+            WriteLine($"PrintMe(dynamic) called with a {value.GetType()} value: {value}");
+        }
+
+        //static void PrintMe(object value)
+        //{
+        //    WriteLine($"PrintMe(object value) = value: {value}");
+        //}
+
+
     }
 }
