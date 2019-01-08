e550e4c7fafe835c9a4dc5d53c039a00e496dbdb
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-01-08 22:26:22 +0100

    - Dynamic C# Fundamentals - -- Simplifying Code with Dynamic C# -- --- Simplifying reflection code ---

---
+++ SymplifyingReflactionCode/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using static System.Console;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvokeMethodUsingReflection();
+            InvokeMethodUsingDynamic();
+
+            WriteLine("\n\nPress enter to exit...");
+            ReadLine();
+        }
+
+        private static void InvokeMethodUsingReflection()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.GetType().InvokeMember("AppendLine",
+                BindingFlags.InvokeMethod,
+                null,
+                sb,
+                new object[] {"Hello Maciej, it is Reflection!"});
+
+
+
+            WriteLine(sb);
+        }
+
+
+        private static void InvokeMethodUsingDynamic()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ((dynamic) sb).AppendLine("Hello Maciej, it is Dynamic!");
+
+
+
+            WriteLine(sb);
+        }
+    }
+
+}
