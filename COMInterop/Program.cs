89b464f40fcb2eae756aff107467ad039bd54781
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-01-23 23:36:52 +0100

    - Dynamic C# Fundamentals - -- Simplifying Code with Dynamic C# -- --- COM Interop without Interop Assemblies ---

---
+++ COMInterop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace COMInterop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type excelType = Type.GetTypeFromProgID("Excel.Application", true);
+
+            dynamic excel = Activator.CreateInstance(excelType);
+
+            excel.Visible = true;
+            excel.Workbooks.Add();
+
+            dynamic defaultWorkSheet = excel.ActiveSheet;
+            defaultWorkSheet.Cells[1, "A"] = "This is the Name Column";
+            defaultWorkSheet.Columns[1].AutoFit();
+
+            WriteLine("\n\nPress enter to exit...");
+            ReadLine();
+
+        }
+    }
+}
