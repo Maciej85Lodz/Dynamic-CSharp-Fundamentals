164eeb2b94f3625d47346d3befc79e783c64d5f4
maciej.wolejszo <maciej.wolejszo@gmail.com>
2019-01-24 00:11:13 +0100

    - Dynamic C# Fundamentals - …  … -- Simplifying Code with Dynamic C# -- --- Dynamically Populating Excel with Arbitaty JSON Data ---

---
+++ DynamicJSON_EXCEL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using static System.Console;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic jsonData = JsonConvert.DeserializeObject(File.ReadAllText("Customers.json"));
+
+            dynamic excel = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application", throwOnError: true));
+            excel.Visible = true;
+            excel.Workbooks.Add();
+            dynamic defaultWorksheet = excel.ActiveSheet;
+
+            int currentRow = 1;
+            foreach (dynamic Customer in jsonData.Customers)
+            {
+                defaultWorksheet.Cells[currentRow, "A"] = Customer.firstName;
+                defaultWorksheet.Cells[currentRow, "B"] = Customer.secondName;
+                currentRow++;
+            }
+
+            WriteLine("\n\nPress enter to exit...");
+            ReadLine();
+        }
+    }
+}
